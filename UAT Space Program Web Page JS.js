4b5e1d9f7f73fc3d8e94c443e4b93e8a58151bd0
McWheelin <105450437+McWheelin@users.noreply.github.com>
2022-06-12 09:35:30 -0700

    Add files via upload

---
+++ UAT Space Program Web Page JS.js
@@ -0,0 +1,36 @@
+ /*
+  Justice McNeeley
+  6/12/22
+  UAT Space Program Assignment
+*/
+
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Space Force" && password == "HotWheels7733"){
+alert ("Login successfully");
+window.location.href = "UAT Space Program Web Page Main Page HTML2.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("login").disabled = true;
+return false;
+}
+}
+}
+
+
+
+        $(window).on("load resize ", function() {
+            var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+            $('.tbl-header').css({'padding-right':scrollWidth});
+          }).resize();
